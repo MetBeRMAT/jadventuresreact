ffd68ad1397fa4e043e64e37a2767d3fe18726de
Mattia Bernardi <bernardi.mattia@libero.it>
2024-02-22 14:53:36 +0100

    navbar completata

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -6,23 +6,19 @@ const Navbar = () =>
         <>
         <nav class="navbar navbar-expand-lg bg-light mb-4 sticky-top">
             <div class="container-fluid">
-                <Link class="navbar-brand" to="/" >HOMEPAGE</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        {/* <Link class="nav-link active" to="/allpeople" >ALL PEOPLE</Link> */}
+                    <li class="nav-item ms-5 p-3 fw-bold">
+                        <Link class="nav-link" to="/">AllQuests</Link>
                     </li>
-                    <li class="nav-item">
-                    <Link class="nav-link" to="/login">Login</Link>
+                    <li class="nav-item p-3 fw-bold position-absolute top-50 start-50 translate-middle">
+                        <Link class="nav-link disabled">MyQuest</Link>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <li class="nav-item p-3 me-5 position-absolute top-0 end-0 fw-bold">
+                        <Link class="nav-link" to="/login">Login</Link>
                     </li>
                 </ul>
                 </div>
