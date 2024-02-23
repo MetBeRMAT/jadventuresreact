c961b12a8301188381837c6f6b2b36bff633799c
Mattia Bernardi <bernardi.mattia@libero.it>
2024-02-23 09:19:34 +0100

    cambio navbar del MyQuest

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () =>
 {
+    let logged = true;
+    let showMyQuest = "nav-link";
+    let dontShowMyQuest = "nav-link disabled";
+
     return(
         <>
         <nav class="navbar navbar-expand-lg bg-light mb-4 sticky-top">
@@ -18,7 +22,7 @@ const Navbar = () =>
                         <Link class="nav-link disabled">MyQuest</Link>
                     </li>
                     <li class="nav-item p-3 fw-bold position-absolute top-50 start-50 translate-middle">
-                        <Link class="nav-link" to="/MyQuestsPage">MyQuest</Link>
+                        <Link class={logged ? dontShowMyQuest : showMyQuest} to="/MyQuestsPage">MyQuest</Link>
                     </li>
                     <li class="nav-item p-3 me-5 position-absolute top-0 end-0 fw-bold">
                         <Link class="nav-link" to="/login">Login</Link>
